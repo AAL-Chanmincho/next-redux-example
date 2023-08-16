1b53d8f64aa4ab870378f3d605c58cedd7df3c56
Alchera-TimCho <cm.cho@alcherainc.com>
2023-08-16 14:01:25 +0900

    install redux/toolkit and redux store setup

--- my-app/src/app/store/index.ts
+++ my-app/src/app/store/index.ts
@@ -1,14 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { countSlice } from '@/app/store/count-slice';
+import counterReducer from './counterSlice'
 
-
-// config the store
-const store= configureStore({
+export const store = configureStore({
   reducer: {
-    count: countSlice.reducer
-  }
+    counter: counterReducer,
+  },
 })
 
-// export default the store
-export default store
-
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,7 +1,29 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../app/store/counterSlice'
+import { RootState } from '@/app/store';
+
 export default function Page() {
-  return <div>
-    <button>Increase Counter</button>
-    <button>Decrease Counter</button>
-    <h1>0</h1>
-  </div>
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <h1>{count}</h1>
+      </div>
+    </div>
+  )
 }
